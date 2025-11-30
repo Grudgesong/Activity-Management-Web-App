f462836248b9ca15d59bb20b133cf799a5158122
Sangraj Ranjit <Grudgesong@gmail.com>
2025-11-30 22:08:17 +0800

    CRUD operations BACKEND, Mediator for the Operation, Automapper for edit operation

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +7,11 @@ namespace API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
-    {
-    }
+  {
+    private IMediator? _mediator;
+
+//Activities controller has access to protected properties so that file can access IMediator from here.
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+    ?? throw new InvalidOperationException("Imediator service is unavailable");
+  }
 }
