9e18bd275ff27977f3ffa60eb6b8f120ddcb0feb
Sangraj Ranjit <Grudgesong@gmail.com>
2025-12-10 21:37:19 +0800

    Implemented React query for state management and CRUD operation connected with API

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -14,6 +14,6 @@ public bool isCancelled { get; set; }
 //location props
 public required string City { get; set; }
 public required string Venue { get; set; }
-public required double Latitude { get; set; }
-public required double Longitude { get; set; }
+public double Latitude { get; set; }
+public double Longitude { get; set; }
 }
