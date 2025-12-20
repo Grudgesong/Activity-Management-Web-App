c25b1af1e65e21dad0cc6468e263495d1c4d6f5a
Sangraj Ranjit <Grudgesong@gmail.com>
2025-12-20 20:52:10 +0800

    Handling API error responses

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,17 +1,28 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseApiController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BaseApiController : ControllerBase
   {
     private IMediator? _mediator;
 
-//Activities controller has access to protected properties so that file can access IMediator from here.
+    //Activities controller has access to protected properties so that file can access IMediator from here.
     protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
     ?? throw new InvalidOperationException("Imediator service is unavailable");
+      protected ActionResult HandleResult<T>(Result<T> result)
+    {
+      if (!result.IsSuccess && result.Code == 404) return NotFound();
+
+      if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+
+      return BadRequest(result.Error);
+    }
   }
-}
+
+
+  }
