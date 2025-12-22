ed404161be722213990169037788b2d34e5874f0
Sangraj Ranjit <Grudgesong@gmail.com>
2025-12-22 20:33:33 +0800

    Handling errors in client

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,9 +1,11 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 export const router = createBrowserRouter([
   {
@@ -30,6 +32,19 @@ export const router = createBrowserRouter([
         path: 'manage/:id', 
         element: <ActivityForm />
       },
+      {
+        path: 'not-found', 
+        element: <NotFound />
+      },
+      {
+        path: 'server-error', 
+        element: <ServerError />
+      },
+      {
+        path: '*', 
+        element: <Navigate replace to='/not-found' />
+      },
+      
     ]
   }
 ])
